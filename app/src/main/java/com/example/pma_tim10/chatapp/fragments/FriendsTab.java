50edef293beed2997b0d4b53d568777a66b4057c
Dorian <theory93rk@gmail.com>
2017-04-25 22:20:31 +0200

    basic ui for friends list

--- app/src/main/java/com/example/pma_tim10/chatapp/fragments/FriendsTab.java
+++ app/src/main/java/com/example/pma_tim10/chatapp/fragments/FriendsTab.java
@@ -3,19 +3,45 @@ package com.example.pma_tim10.chatapp.fragments;
 /**
  * Created by Dorian on 4/25/2017.
  */
-import android.support.v4.app.Fragment;
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.example.pma_tim10.chatapp.R;
 
-public class FriendsTab extends Fragment{
+import java.util.ArrayList;
+
+public class FriendsTab extends ListFragment implements AdapterView.OnItemClickListener {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.friends_tab, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("Djuro pucar stari");
+        friends.add("Deda Ignjat");
+        friends.add("Djed Djuro");
+        friends.add("Joja");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,friends);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getActivity(), "Item: " + i, Toast.LENGTH_SHORT).show();
     }
 }
