eaec7b045b00bd926ba7c74011109a20d69bcfbb
Okiljevic Aleksandar <okiljevic93@gmail.com>
2017-06-10 19:52:27 +0200

    Push notifications started

---
+++ app/src/main/java/com/example/pma_tim10/chatapp/notifications/FirebaseIDService.java
@@ -0,0 +1,25 @@
+package com.example.pma_tim10.chatapp.notifications;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Daniel on 6/10/2017.
+ */
+
+public class FirebaseIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = FirebaseIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
